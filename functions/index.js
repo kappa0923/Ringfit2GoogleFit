645a73a5f4ecfca7495b97b05796edf4e284673b
kappa0923 <tom.kappa0923@gmail.com>
2020-05-26 22:15:09 +0900

    OCRした文字群から対象の情報を抽出

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,11 @@ const functions = require('firebase-functions');
 const vision = require('@google-cloud/vision');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const Rectangle = require('./rectangle.js');
+const RECORD_DATE_POINT = { "x": 450, "y": 60 };
+const RECORD_TIME_POINT = { "x": 510, "y": 150 };
+const RECORD_CAL_POINT = { "x": 760, "y": 150 };
+const RECORD_DIST_POINT = { "x": 1015, "y": 150 };
 
 /**
  * Ringfitのスクショをtwitterに投稿したら文字認識してGoogleFitに登録する。
@@ -28,13 +33,6 @@ const getImageUrl = async (url) => {
     console.log(imageUrl);
     return imageUrl;
   })
-
-  // return req.get(options, (err, res, body) => {
-  //   const $ = cheerio.load(body);
-  //   const imageUrl = $("meta[property='og:image']").attr("content")
-  //   console.log(imageUrl);
-  //   return imageUrl;
-  // });
 }
 
 /**
@@ -43,12 +41,35 @@ const getImageUrl = async (url) => {
  */
 const detectTextFromUrl = async (url) => {
   const client = new vision.ImageAnnotatorClient();
-  console.log('Detect image from URL: '+ url);
+  console.log('Detect image from URL: ' + url);
 
   return client.textDetection(url).then(result => {
     console.log(result);
     const detections = result[0].textAnnotations;
+
+    // 認識した文字から必要なデータを探す
     console.log('Text:');
-    detections.forEach(text => console.log(text));
+    const recordDate = detections.filter(text => {
+      const rect = new Rectangle(text.boundingPoly.vertices[0].x, text.boundingPoly.vertices[0].y, text.boundingPoly.vertices[2].x, text.boundingPoly.vertices[2].y);
+      return rect.hasPoint(RECORD_DATE_POINT.x, RECORD_DATE_POINT.y);
+    });
+    const recordTime = detections.filter(text => {
+      const rect = new Rectangle(text.boundingPoly.vertices[0].x, text.boundingPoly.vertices[0].y, text.boundingPoly.vertices[2].x, text.boundingPoly.vertices[2].y);
+      return rect.hasPoint(RECORD_TIME_POINT.x, RECORD_TIME_POINT.y);
+    });
+    const recordCal = detections.filter(text => {
+      const rect = new Rectangle(text.boundingPoly.vertices[0].x, text.boundingPoly.vertices[0].y, text.boundingPoly.vertices[2].x, text.boundingPoly.vertices[2].y);
+      return rect.hasPoint(RECORD_CAL_POINT.x, RECORD_CAL_POINT.y);
+    });
+    const recordDist = detections.filter(text => {
+      const rect = new Rectangle(text.boundingPoly.vertices[0].x, text.boundingPoly.vertices[0].y, text.boundingPoly.vertices[2].x, text.boundingPoly.vertices[2].y);
+      return rect.hasPoint(RECORD_DIST_POINT.x, RECORD_DIST_POINT.y);
+    });
+
+    // 取得した情報
+    console.log('Date: ' + JSON.stringify(recordDate[1].description));
+    console.log('Time: ' + JSON.stringify(recordTime[1].description));
+    console.log('Cal: ' + JSON.stringify(recordCal[1].description));
+    console.log('Distinct: ' + JSON.stringify(recordDist[1].description));
   });
 }
